ace73fd7d8729476445af33b67f23ee573c9b612
Grzegorz Adam Hankiewicz <gradha@argon.sabren.com>
2011-07-09 18:21:03 +0200

    Corrected UINavigationController source code include.

--- src/UINavigationController+ELHASO.m
+++ src/UINavigationController+ELHASO.m
@@ -1,11 +1,11 @@
-#import "categories/UINavigationController+ELHASO.h"
+#import "UINavigationController+ELHASO.h"
 
 @implementation UINavigationController (ELHASO)
 
 /** Unlike topViewController, this returns the controller at the base.
  * Returns nil if there was no controller available.
  */
-- (UIViewController*)rootController;
+- (UIViewController*)rootController
 {
 	if (self.viewControllers.count > 0)
 		return [self.viewControllers objectAtIndex:0];
