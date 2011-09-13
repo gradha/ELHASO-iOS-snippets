215a6e09d837be419dd8d4449b7389d4f794cde9
Grzegorz Adam Hankiewicz <gradha@argon.sabren.com>
2011-09-13 22:15:18 +0200

    Added safeguards to after:perform:.

--- src/NSObject+ELHASO.m
+++ src/NSObject+ELHASO.m
@@ -1,5 +1,7 @@
 #import "NSObject+ELHASO.h"
 
+#import "ELHASO.h"
+
 @implementation NSObject (ELHASO)
 
 /** Runs a block after a specified amount of time.
@@ -8,9 +10,14 @@
  * http://stackoverflow.com/questions/4007023/blocks-instead-of-performselectorwithobjectafterdelay,
  * which was based on Mike Ash's code. This doesn't use GDC at all, so it
  * depends on run loops and everything else related.
+ *
+ * Remember that since this uses a runloop to queue the block execution, this
+ * has to be run from a thread with a run loop (ie. the method will assert when
+ * not run from the ui thread).
  */
 - (void)after:(NSTimeInterval)delay perform:(void (^)(void))block
 {
+	BLOCK_UI();
 	block = [[block copy] autorelease];
 	[self performSelector:@selector(run_block:)
 		withObject:block afterDelay:delay];
